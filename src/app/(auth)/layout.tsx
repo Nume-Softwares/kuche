8830152000dcc500f07659db1670411b07b8e647
Ewerton Igor <ewertonigor@icloud.com>
2025-01-21 19:26:36 -0300

    feat: sidebar, modetoggle and sheet notifications

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,16 @@
 import { SearchCommand } from '@/components/auth/search-command'
 import { AppSidebar } from '@/components/auth/siderbar/sidebarMain'
-import { SidebarProvider } from '@/components/ui/sidebar'
+import { ModeToggle } from '@/components/themes/mode-toggle'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { Bell } from 'lucide-react'
 
 export default function AuthLayout({
   children,
@@ -12,7 +22,34 @@ export default function AuthLayout({
       <AppSidebar />
       <SearchCommand />
 
-      <main className="w-full">{children}</main>
+      <main className="w-full">
+        <header className="flex justify-between px-8 pt-4">
+          <SidebarTrigger className="size-8 border bg-white dark:bg-zinc-950 dark:hover:bg-neutral-800" />
+
+          <div className="flex gap-2">
+            <ModeToggle />
+            <Sheet>
+              <SheetTrigger className="relative flex size-8 items-center justify-center rounded-md border hover:bg-zinc-100">
+                <Bell className="size-4" />
+                <span className="absolute right-0 top-0 flex size-5 -translate-y-1/2 translate-x-1/2 items-center justify-center rounded-full bg-green-600 text-xs font-bold text-white">
+                  32
+                </span>
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Are you absolutely sure?</SheetTitle>
+                  <SheetDescription>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our servers.
+                  </SheetDescription>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </header>
+
+        {children}
+      </main>
     </SidebarProvider>
   )
 }

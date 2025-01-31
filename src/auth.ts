9c0476ad3a6c4b76b8b3c165740d0c0cfd6472aa
Ewerton Igor <ewertonigor@icloud.com>
2025-01-31 08:50:17 -0300

    feat: callback sign google

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 import Google from 'next-auth/providers/google'
+import { cookies } from 'next/headers'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -25,4 +26,51 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
     Google,
   ],
+  callbacks: {
+    async signIn({ user, account }) {
+      // Verifica se o login foi feito com o Google
+      if (account?.provider === 'google') {
+        try {
+          // Chama o endpoint do backend para verificar o e-mail e obter o token JWT
+          const backendResponse = await fetch(
+            'http://localhost:3333/restaurant/member/google',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                email: user.email,
+                restaurantId: process.env.RESTAURANT_ID,
+              }),
+            },
+          )
+
+          if (!backendResponse.ok) {
+            return false
+          }
+
+          const { access_token } = await backendResponse.json()
+
+          const cookieStore = await cookies()
+
+          cookieStore.set('jwt', access_token, {
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: 60 * 60 * 24,
+            path: '/',
+          })
+        } catch (error) {
+          console.error('Erro ao fazer login com o Google:', error)
+          return false
+        }
+      }
+
+      return true
+    },
+
+    authorized: async ({ auth }) => {
+      return !!auth
+    },
+  },
 })
